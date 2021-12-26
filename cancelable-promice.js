b0a75d8244d905fa1d434c68e5e419b2cd02f454
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-12-26 18:12:24 +0200

    test

---
+++ cancelable-promice.js
@@ -0,0 +1,37 @@
+class CancelablePromise {
+  constructor(executor, promise = null, chain = []) {
+    if (typeof executor !== 'function' && promise === null)
+      throw new Error('Promise executor must be fucntion')
+    
+    this.isCanceled = false
+    this.promise = promise || new Promise((res, rej) => executor(val => {
+      if (this.isCanceled) rej(this)
+      res(val)
+    }, rej)
+    )
+    this.chain = chain
+    this.chain.push(this)
+  }
+
+  then(onFulfilled, onRejected) {
+    if (!onFulfilled && !onRejected)
+      return this
+    if (typeof onFulfilled !== 'function' && onFulfilled !== undefined)
+      throw new Error('Expect first argument to be a function or undefined')
+    
+    const newPromise = this.promise
+      .then(onFulfilled, onRejected).catch(onRejected)
+    return new CancelablePromise(null, newPromise, this.chain)
+  }
+
+  catch(onRejected) {
+    return this.then(undefined, onRejected)
+  }
+
+  cancel() {
+    this.chain.forEach(promise => (promise.isCanceled = true))
+    return this
+  }
+}
+
+module.exports = CancelablePromise
